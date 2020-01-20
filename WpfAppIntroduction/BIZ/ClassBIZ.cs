ac04c22217a718176891637c84fdf3b75f40b0de
KianTM <kian0949@edu.campusvejle.dk>
2020-01-20 13:09:37 +0100

    Add project files.

---
+++ WpfAppIntroduction/BIZ/ClassBIZ.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfAppIntroduction.BIZ
+{
+    class ClassBIZ
+    {
+        public ClassBIZ()
+        {
+
+        }
+
+        public void Delopgave12(ListBox listBox)
+        {
+            for (int i = 4711; i <= 4736; i++)
+            {
+                listBox.Items.Add(i.ToString());
+            }
+        }
+    }
+}
---
+++ WpfAppIntroduction/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppIntroduction.BIZ;
+
+namespace WpfAppIntroduction
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ClassBIZ classBIZ = new ClassBIZ();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpg12_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.Delopgave12(listBoxRes);
+        }
+    }
+}
